f12a4ce96e073c0cba6c519bad167adb8f9a0770
kaua <kaua.santanaj@gmail.com>
2026-07-04 20:14:56 -0300

    first commit

---
+++ src/test/java/com/api/cortex/CortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
